670f5296918403e8bb8935ec8b9b137e7dd839cb
Netanel98 <netanelsoussan@gmail.com>
2024-09-02 18:32:21 +0300

    git commit -m "Initial commit"

--- app/src/main/java/com/example/readingbooks/BookDetailsActivity.kt
+++ app/src/main/java/com/example/readingbooks/BookDetailsActivity.kt
@@ -1,47 +1,103 @@
 package com.example.readingbooks
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.readingbooks.ui.theme.ReadingListTheme
+import android.content.Intent
+import android.net.Uri
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.squareup.picasso.Picasso
 
-class BookDetailsActivity : ComponentActivity() {
+class BookDetailsActivity : AppCompatActivity() {
+
+    // creating variables for strings,text view,
+    // image views and button.
+    lateinit var titleTV: TextView
+    lateinit var subtitleTV: TextView
+    lateinit var publisherTV: TextView
+    lateinit var descTV: TextView
+    lateinit var pageTV: TextView
+    lateinit var publisherDateTV: TextView
+    lateinit var previewBtn: Button
+    lateinit var buyBtn: Button
+    lateinit var bookIV: ImageView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            ReadingListTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+        setContentView(R.layout.activity_book_details)
+
+        // initializing our variables.
+        titleTV = findViewById(R.id.idTVTitle)
+        subtitleTV = findViewById(R.id.idTVSubTitle)
+        publisherTV = findViewById(R.id.idTVpublisher)
+        descTV = findViewById(R.id.idTVDescription)
+        pageTV = findViewById(R.id.idTVNoOfPages)
+        publisherDateTV = findViewById(R.id.idTVPublishDate)
+        previewBtn = findViewById(R.id.idBtnPreview)
+        buyBtn = findViewById(R.id.idBtnBuy)
+        bookIV = findViewById(R.id.idIVbook)
+
+        // getting the data which we have passed from our adapter class.
+        val title = getIntent().getStringExtra("title")
+        val subtitle = getIntent().getStringExtra("subtitle")
+        val publisher = getIntent().getStringExtra("publisher")
+        val publishedDate = getIntent().getStringExtra("publishedDate")
+        val description = getIntent().getStringExtra("description")
+        val pageCount = getIntent().getIntExtra("pageCount", 0)
+        val thumbnail = getIntent().getStringExtra("thumbnail")
+        val previewLink = getIntent().getStringExtra("previewLink")
+        val infoLink = getIntent().getStringExtra("infoLink")
+        val buyLink = getIntent().getStringExtra("buyLink")
+
+        // after getting the data we are setting
+        // that data to our text views and image view.
+        titleTV.setText(title)
+        subtitleTV.setText(subtitle)
+        publisherTV.setText(publisher)
+        publisherDateTV.setText("Published On : " + publishedDate)
+        descTV.setText(description)
+        pageTV.setText("No Of Pages : " + pageCount)
+        Picasso.get().load(thumbnail).into(bookIV)
+
+        // adding on click listener for our preview button.
+        previewBtn.setOnClickListener {
+            if (previewLink.isNullOrEmpty()) {
+                // below toast message is displayed
+                // when preview link is not present.
+                Toast.makeText(
+                    this@BookDetailsActivity,
+                    "No preview Link present",
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
             }
+            // if the link is present we are opening
+            // that link via an intent.
+            val uri: Uri = Uri.parse(previewLink)
+            val i = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(i)
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+        // adding click listener for buy button
+        buyBtn.setOnClickListener {
+            if (buyLink.isNullOrEmpty()) {
+                // below toast message is displaying
+                // when buy link is empty.
+                Toast.makeText(
+                    this@BookDetailsActivity,
+                    "No buy page present for this book",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            // if the link is present we are opening
+            // the link via an intent.
+            val uri = Uri.parse(buyLink)
+            val i = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(i)
+        }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ReadingListTheme {
-        Greeting("Android")
     }
 }
