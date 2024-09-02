9e3c23c7fd89f8e19ff4a764ab23ff1caa0ec9b2
Netanel98 <netanelsoussan@gmail.com>
2024-09-02 20:49:41 +0300

    git commit -m "Initial commit"

--- app/src/main/java/com/example/readingbooks/BookRVAdapter.kt
+++ app/src/main/java/com/example/readingbooks/BookRVAdapter.kt
@@ -1,4 +1,14 @@
 package com.example.readingbooks
 
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+
 class BookRVAdapter(booksList: ArrayList<BookRVModal>, mainActivity: MainActivity) {
 }
