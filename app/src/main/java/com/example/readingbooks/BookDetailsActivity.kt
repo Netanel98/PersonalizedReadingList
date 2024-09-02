7cd25c1891bf87f0b8c15a9a697cb485d154fe0e
Netanel98 <netanelsoussan@gmail.com>
2024-09-02 18:33:38 +0300

    git commit -m "Initial commit"

--- app/src/main/java/com/example/readingbooks/BookDetailsActivity.kt
+++ app/src/main/java/com/example/readingbooks/BookDetailsActivity.kt
@@ -8,6 +8,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.readinglist.R
 import com.squareup.picasso.Picasso
 
 class BookDetailsActivity : AppCompatActivity() {
