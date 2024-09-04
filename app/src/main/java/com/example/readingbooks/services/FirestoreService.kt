eb620d6de318c648648a85a050004f8fe1bc175f
Netanel98 <netanelsoussan@gmail.com>
2024-09-04 23:13:19 +0300

    git commit -m "Initial commit"

--- app/src/main/java/com/example/readingbooks/services/FirestoreService.kt
+++ app/src/main/java/com/example/readingbooks/services/FirestoreService.kt
@@ -2,30 +2,23 @@ package com.example.readingbooks.services
 
 import com.example.readingbooks.models.Book
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.ktx.toObject
 import kotlinx.coroutines.tasks.await
 
 class FirestoreService {
     private val db = FirebaseFirestore.getInstance()
 
-    suspend fun addBook(book: Book) {
-        db.collection("books").add(book).await()
+    // Add a book to Firestore and return the document ID
+    suspend fun addBook(book: Book): String {
+        val document = db.collection("books").add(book).await()
+        return document.id  // Return the new document ID
     }
 
-    suspend fun getBooks(): List<Book> {
-        return db.collection("books")
-            .get()
-            .await()
-            .documents
-            .mapNotNull { it.toObject<Book>() }
-    }
-
+    // Update a book in Firestore
     suspend fun updateBook(book: Book) {
-        book.id?.let {
-            db.collection("books").document(it.toString()).set(book).await()
-        }
+        book.id?.let { db.collection("books").document(it.toString()).set(book).await() }
     }
 
+    // Delete a book from Firestore
     suspend fun deleteBook(bookId: String) {
         db.collection("books").document(bookId).delete().await()
     }
