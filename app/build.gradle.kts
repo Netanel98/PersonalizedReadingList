5aed4b770bc4c495b2382999079a740f3b2d0634
Netanel98 <netanelsoussan@gmail.com>
2024-09-04 23:45:16 +0300

    git commit -m "Initial commit"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,6 +75,7 @@ dependencies {
     implementation(libs.library)
     implementation(libs.androidx.camera.core)
     implementation(libs.firebase.storage.ktx)
+    implementation(libs.androidx.navigation.fragment)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
