cafec6999ca90352b57799da0f28c2cc1e223964
Netanel98 <netanelsoussan@gmail.com>
2024-09-04 15:55:35 +0300

    git commit -m "Initial commit"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.room.common)
     implementation(libs.androidx.room.ktx)
+    implementation(libs.firebase.firestore.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/example/readingbooks/services/FirestoreService.kt
@@ -0,0 +1,32 @@
+package com.example.readingbooks.services
+
+import com.example.readingbooks.models.Book
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.toObject
+import kotlinx.coroutines.tasks.await
+
+class FirestoreService {
+    private val db = FirebaseFirestore.getInstance()
+
+    suspend fun addBook(book: Book) {
+        db.collection("books").add(book).await()
+    }
+
+    suspend fun getBooks(): List<Book> {
+        return db.collection("books")
+            .get()
+            .await()
+            .documents
+            .mapNotNull { it.toObject<Book>() }
+    }
+
+    suspend fun updateBook(book: Book) {
+        book.id?.let {
+            db.collection("books").document(it).set(book).await()
+        }
+    }
+
+    suspend fun deleteBook(bookId: String) {
+        db.collection("books").document(bookId).delete().await()
+    }
+}
