0040fe447eca6b4c1f7666f35eaad6c10372798c
Netanel98 <netanelsoussan@gmail.com>
2024-09-04 15:12:57 +0300

    git commit -m "Initial commit"

---
+++ app/src/main/java/com/example/readingbooks/data/AppDatabase.kt
@@ -0,0 +1,30 @@
+package com.example.readingbooks.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.readingbooks.models.Book
+
+@Database(entities = [Book::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun bookDao(): BookDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "reading_list_database"
+                ).fallbackToDestructiveMigration()
+                    .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
