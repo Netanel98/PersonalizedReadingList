411af43ace5f11281108ecb871401105f36a328e
Netanel98 <netanelsoussan@gmail.com>
2024-09-20 17:41:09 +0300

    git commit -m "Initial commit"

--- app/src/main/java/com/example/readingbooks/BookRVAdapter.kt
+++ app/src/main/java/com/example/readingbooks/BookRVAdapter.kt
@@ -1,40 +1,76 @@
 package com.example.readingbooks
-
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.readingbooks.databinding.ItemBookBinding
-import com.example.readingbooks.models.Book
+import com.bumptech.glide.Glide
 
-class BookRVAdapter : ListAdapter<Book, BookRVAdapter.BookViewHolder>(BookDiffCallback()) {
-
-    class BookViewHolder(private val binding: ItemBookBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(book: Book) {
-            binding.tvTitle.text = book.title
-            binding.tvAuthor.text = book.author
-        }
+class BookRVAdapter(
+    // on below line we are passing variables
+    // as course list and context
+    private var bookList: ArrayList<BookRVModal>,
+    private var ctx: Context
+) : RecyclerView.Adapter<BookRVAdapter.BookViewHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): BookRVAdapter.BookViewHolder {
+        // this method is use to inflate the layout file
+        // which we have created for our recycler view.
+        // on below line we are inflating our layout file.
+        val itemView = LayoutInflater.from(parent.context).inflate(
+            R.layout.item_book,
+            parent, false
+        )
+        // at last we are returning our view holder
+        // class with our item View File.
+        return BookRVAdapter.BookViewHolder(itemView)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val binding = ItemBookBinding.inflate(layoutInflater, parent, false)
-        return BookViewHolder(binding)
-    }
+    override fun onBindViewHolder(holder: BookRVAdapter.BookViewHolder, position: Int) {
+        val bookInfo = bookList.get(position)
+        // below line is use to set image from URL in our image view.
+        Picasso.get().load(bookInfo.thumbnail).into(holder.bookIV);
+        holder.bookTitleTV.text = bookInfo.title
+        holder.bookPagesTV.text = "Pages : " + bookInfo.pageCount
 
-    override fun onBindViewHolder(holder: BookViewHolder, position: Int) {
-        val book = getItem(position)
-        holder.bind(book)
-    }
+        // below line is use to add on click listener for our item of recycler view.
+        holder.itemView.setOnClickListener {
+            // inside on click listener method we are calling a new activity
+            // and passing all the data of that item in next intent.
+            val i = Intent(ctx, BookDetailsActivity::class.java)
+            i.putExtra("title", bookInfo.title)
+            i.putExtra("subtitle", bookInfo.subtitle)
+            i.putExtra("authors", bookInfo.authors)
+            i.putExtra("publisher", bookInfo.publisher)
+            i.putExtra("publishedDate", bookInfo.publishedDate)
+            i.putExtra("description", bookInfo.description)
+            i.putExtra("pageCount", bookInfo.pageCount)
+            i.putExtra("thumbnail", bookInfo.thumbnail)
+            i.putExtra("previewLink", bookInfo.previewLink)
+            i.putExtra("infoLink", bookInfo.infoLink)
+            i.putExtra("buyLink", bookInfo.buyLink)
 
-    class BookDiffCallback : DiffUtil.ItemCallback<Book>() {
-        override fun areItemsTheSame(oldItem: Book, newItem: Book): Boolean {
-            return oldItem.id == newItem.id
+            // after passing that data we are
+            // starting our new  intent.
+            ctx.startActivity(i)
         }
 
-        override fun areContentsTheSame(oldItem: Book, newItem: Book): Boolean {
-            return oldItem == newItem
-        }
+    }
+
+    override fun getItemCount(): Int {
+        return bookList.size
+    }
+
+    class BookViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        // on below line we are initializing our
+        // course name text view and our image view.
+        val bookTitleTV: TextView = itemView.findViewById(R.id.tvTitle)
+        val bookPagesTV: TextView = itemView.findViewById(R.id.idTVBookPages)
+        val bookIV: ImageView = itemView.findViewById(R.id.bookCoverImageView)
     }
 }
