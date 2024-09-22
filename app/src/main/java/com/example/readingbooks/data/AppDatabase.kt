074017e988783dba452c699f6aa55db051577784
Netanel98 <netanelsoussan@gmail.com>
2024-09-22 09:25:24 +0300

    git commit -m "Initial commit"

--- app/src/main/java/com/example/readingbooks/data/AppDatabase.kt
+++ app/src/main/java/com/example/readingbooks/data/AppDatabase.kt
@@ -24,7 +24,7 @@ abstract class AppDatabase : RoomDatabase() {
                     context.applicationContext,
                     AppDatabase::class.java,
                     "reading_list_database"
-                ).fallbackToDestructiveMigration()
+                ).fallbackToDestructiveMigration(true)
                     .build()
                 INSTANCE = instance
                 instance
