ff64fcb9fc49374938b3a4c3350242811d340a94
Netanel98 <netanelsoussan@gmail.com>
2024-09-23 14:46:23 +0300

    git commit -m "Initial commit"

--- app/src/main/java/com/example/readingbooks/LibraryFragment.kt
+++ app/src/main/java/com/example/readingbooks/LibraryFragment.kt
@@ -1,11 +1,14 @@
 package com.example.readingbooks
+
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.ProgressBar
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.android.volley.Request
@@ -13,70 +16,74 @@ import com.android.volley.RequestQueue
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.example.readingbooks.models.Book
+import com.example.readingbooks.R
+import org.json.JSONObject
 
-class LibraryFragment : AppCompatActivity() {
-    // on below line we are creating variables.
-    lateinit var mRequestQueue: RequestQueue
-    lateinit var booksList: ArrayList<Book>
-    lateinit var loadingPB: ProgressBar
-    lateinit var searchEdt: EditText
-    lateinit var searchBtn: ImageButton
+class LibraryFragment : Fragment() {
+    private lateinit var mRequestQueue: RequestQueue
+    private lateinit var booksList: ArrayList<Book>
+    private lateinit var loadingPB: ProgressBar
+    private lateinit var searchEdt: EditText
+    private lateinit var searchBtn: ImageButton
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_library)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_library, container, false)
+    }
 
-        // on below line we are initializing
-        // our variable with their ids.
-        loadingPB = findViewById(R.id.idLoadingPB)
-        searchEdt = findViewById(R.id.idEdtSearchBooks)
-        searchBtn = findViewById(R.id.idBtnSearch)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // initializing our variables with their ids
+        loadingPB = view.findViewById(R.id.idLoadingPB)
+        searchEdt = view.findViewById(R.id.idEdtSearchBooks)
+        searchBtn = view.findViewById(R.id.idBtnSearch)
 
         // adding click listener for search button
         searchBtn.setOnClickListener {
             loadingPB.visibility = View.VISIBLE
-            // checking if our edittext field is empty or not.
+            // checking if our edittext field is empty or not
             if (searchEdt.text.toString().isNullOrEmpty()) {
-                searchEdt.setError("Please enter search query")
+                searchEdt.error = "Please enter search query"
+                loadingPB.visibility = View.GONE
+            } else {
+                // if the search query is not empty then we are
+                // calling get books data method to load all
+                // the books from the API
+                getBooksData(searchEdt.text.toString())
             }
-            // if the search query is not empty then we are
-            // calling get book info method to load all
-            // the books from the API.
-            getBooksData(searchEdt.getText().toString());
         }
-
     }
 
     private fun getBooksData(searchQuery: String) {
-        // creating a new array list.
+        // creating a new array list
         booksList = ArrayList()
 
-        // below line is use to initialize
-        // the variable for our request queue.
-        mRequestQueue = Volley.newRequestQueue(this@LibraryFragment)
-
-        // below line is use to clear cache this
-        // will be use when our data is being updated.
+        // below line is to initialize
+        // the variable for our request queue
+        mRequestQueue = Volley.newRequestQueue(requireContext())
         mRequestQueue.cache.clear()
 
-        // below is the url for getting data from API in json format.
+        // url for getting data from API in json format
         val url = "https://www.googleapis.com/books/v1/volumes?q=$searchQuery"
 
-        // below line we are  creating a new request queue.
-        val queue = Volley.newRequestQueue(this@LibraryFragment)
-
-        // on below line we are creating a variable for request
-        // and initializing it with json object request
+        // creating a new request queue
         val request = JsonObjectRequest(Request.Method.GET, url, null, { response ->
-            loadingPB.setVisibility(View.GONE);
-            // inside on response method we are extracting all our json data.
+            loadingPB.visibility = View.GONE
+            // inside on response method we are extracting all our json data
             try {
                 val itemsArray = response.getJSONArray("items")
                 for (i in 0 until itemsArray.length()) {
                     val itemsObj = itemsArray.getJSONObject(i)
                     val volumeObj = itemsObj.getJSONObject("volumeInfo")
-                    val id = itemsObj.getInt("id")
-                    val imageUrl = volumeObj.optString("image")
+                    val id = itemsObj.getString("id")
+                    val imageUrl = volumeObj.optString("imageLinks").let {
+                        if (it.isNotEmpty()) JSONObject(it).optString("thumbnail")
+                        else ""
+                    }
                     val title = volumeObj.optString("title")
                     val subtitle = volumeObj.optString("subtitle")
                     val authorsArray = volumeObj.getJSONArray("authors")
@@ -84,8 +91,6 @@ class LibraryFragment : AppCompatActivity() {
                     val publishedDate = volumeObj.optString("publishedDate")
                     val description = volumeObj.optString("description")
                     val pageCount = volumeObj.optInt("pageCount")
-                    val imageLinks = volumeObj.optJSONObject("imageLinks")
-                    val thumbnail = imageLinks?.optString("thumbnail")
                     val previewLink = volumeObj.optString("previewLink")
                     val infoLink = volumeObj.optString("infoLink")
                     val saleInfoObj = itemsObj.optJSONObject("saleInfo")
@@ -93,14 +98,12 @@ class LibraryFragment : AppCompatActivity() {
                     val authorsArrayList: ArrayList<String> = ArrayList()
                     if (authorsArray.length() != 0) {
                         for (j in 0 until authorsArray.length()) {
-                            authorsArrayList.add(authorsArray.optString(i))
+                            authorsArrayList.add(authorsArray.optString(j))
                         }
                     }
 
-                    // after extracting all the data we are
-                    // saving this data in our modal class.
                     val bookInfo = Book(
-                        id,
+                        id.toInt(),
                         imageUrl,
                         title,
                         subtitle,
@@ -109,43 +112,25 @@ class LibraryFragment : AppCompatActivity() {
                         publishedDate,
                         description,
                         pageCount,
-                        thumbnail.toString(),
+                        imageUrl,
                         previewLink,
                         infoLink,
-                        buyLink.toString()
+                        buyLink ?: ""
                     )
-
-                    // below line is use to pass our modal
-                    // class in our array list.
                     booksList.add(bookInfo)
-
-                    // below line is use to pass our
-                    // array list in adapter class.
-                    val adapter = BookRVAdapter(booksList, this@LibraryFragment)
-
-                    // below line is use to add linear layout
-                    // manager for our recycler view.
-                    val layoutManager = GridLayoutManager(this, 3)
-                    val mRecyclerView = findViewById<RecyclerView>(R.id.idRVBooks) as RecyclerView
-
-                    // in below line we are setting layout manager and
-                    // adapter to our recycler view.
-                    mRecyclerView.layoutManager = layoutManager
-                    mRecyclerView.adapter = adapter
                 }
+                val adapter = BookRVAdapter(booksList, requireContext())
+                val mRecyclerView = view?.findViewById<RecyclerView>(R.id.idRVBooks)
+                mRecyclerView?.layoutManager = GridLayoutManager(requireContext(), 3)
+                mRecyclerView?.adapter = adapter
             } catch (e: Exception) {
                 e.printStackTrace()
             }
-
-
         }, { error ->
-            // in this case we are simply displaying a toast message.
-            Toast.makeText(this@LibraryFragment, "No books found..", Toast.LENGTH_SHORT)
-                .show()
+            // in this case we are simply displaying a toast message
+            Toast.makeText(requireContext(), "Failed to load data..", Toast.LENGTH_SHORT).show()
+            loadingPB.visibility = View.GONE
         })
-        // at last we are adding our
-        // request to our queue.
-        queue.add(request)
-
+        mRequestQueue.add(request)
     }
 }
