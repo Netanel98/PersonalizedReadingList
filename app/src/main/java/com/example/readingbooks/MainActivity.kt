0d0b319ca88788dd89d5f6023451dd31bdee8a16
Netanel98 <netanelsoussan@gmail.com>
2024-09-27 18:09:41 +0300

    git commit -m "Initial commit"

--- app/src/main/java/com/example/readingbooks/MainActivity.kt
+++ app/src/main/java/com/example/readingbooks/MainActivity.kt
@@ -1,92 +1,84 @@
 package com.example.readingbooks
 
-import android.net.Uri
+
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.util.Log
-import androidx.activity.result.contract.ActivityResultContracts
+import android.view.View
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isVisible
-import androidx.lifecycle.MutableLiveData
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.cancel
-import com.example.readingbooks.R
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
-import com.example.readingbooks.databinding.ActivityMainBinding
+import androidx.navigation.fragment.findNavController
+import com.example.readingbooks.data.AppDatabase
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var bottomNavigationView: BottomNavigationView
-    private lateinit var navHostFragment: NavHostFragment
-    private lateinit var navController: NavController
-    private lateinit var binding: ActivityMainBinding
-    private var auth: FirebaseAuth = FirebaseAuth.getInstance()
-
-
-    private val scope = CoroutineScope(Dispatchers.IO + Job())
-    var uriResult: MutableLiveData<Uri?> = MutableLiveData<Uri?>()
-    val requestPermission =
-        registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
-            if (uri != null) {
-                uriResult.value = uri
-                Log.d("Picturerequest", "$uri")
-            } else {
-                Log.d("Picturerequest", "No media selected")
-            }
-        }
+    lateinit var navigationMenu: BottomNavigationView
+    lateinit var localDb: AppDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        enableEdgeToEdge()
+        localDb = AppDatabase.getDatabase(applicationContext)
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        navigationMenu = findViewById(R.id.bottom_navigation)
 
-        bottomNavigationView = binding.bottomNavigationView
-        navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
-        navController = navHostFragment.navController
+        if (FirebaseAuth.getInstance().currentUser != null) {
+            navigationMenu.visibility = View.VISIBLE
+        }
+        setupNavigationMenu()
+    }
 
-        NavigationUI.setupWithNavController(
-            bottomNavigationView,navController
-        )
+    private fun setupNavigationMenu() {
+        val navController = getNavController()
+        navigationMenu = findViewById(R.id.bottom_navigation)
 
-        if (isLoggedin()) {
-            navController.navigate(R.id.libraryFragment)
-        } else {
-            navController.navigate(R.id.loginFragment)
+        FirebaseAuth.getInstance().addAuthStateListener { auth ->
+            if (auth.currentUser == null) {
+                navigationMenu.visibility = View.GONE
+                navController.navigate(R.id.loginFragment)
+            } else {
+                navigationMenu.visibility = View.VISIBLE
+                navigationMenu.selectedItemId = R.id.libraryFragment
+            }
+        }
+        navigationMenu.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.profileFragment -> {
+                    navController.navigate(R.id.profileFragment)
+                    true
+                }
+
+                R.id.libraryFragment -> {
+                    navController.navigate(R.id.libraryFragment)
+                    true
+                }
+
+                R.id.booksListFragment -> {
+                    navController.navigate(R.id.booksListFragment)
+                    true
+                }
+                R.id.settingsFragment -> {
+                    navController.navigate(R.id.settingsFragment)
+                    true
+                }
+
+                else -> false
+            }
         }
     }
-    public override fun onStop() {
-        super.onStop()
-        scope.cancel()
-    }
 
-    fun isLoggedin(): Boolean {
-        return auth.currentUser != null
-    }
-
-    fun hideNavBar() {
-        binding.bottomAppBar.isVisible = false
-    }
-
-    fun displayNavBar() {
-        binding.bottomAppBar.isVisible = true
-    }
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        val selectedItemId = bottomNavigationView.selectedItemId
-        outState.putInt("SELECTED_ITEM_ID", selectedItemId)
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        val selectedItemId = savedInstanceState.getInt("SELECTED_ITEM_ID", R.id.libraryFragment)
-        bottomNavigationView.selectedItemId = selectedItemId
+    fun getNavController(): NavController {
+        val navHost =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        return navHost.navController
     }
 }
